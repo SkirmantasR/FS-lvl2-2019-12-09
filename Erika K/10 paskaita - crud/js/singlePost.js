d07ff756ced57a7ab14efd22f9d6252f16353f18
Zilwa <zilvinas.vidmantas.93@gmail.com>
2020-01-07 20:10:28 +0200

    Post's user and user info

--- Erika K/10 paskaita - crud/js/singlePost.js
+++ Erika K/10 paskaita - crud/js/singlePost.js
@@ -1,12 +1,28 @@
-let urlString = window.location.search;
-let index = urlString.indexOf('=');
-let postId = Number(urlString.slice(index + 1));
-console.log(postId);
+const postContainer = document.querySelector('.js-post');
 
-const singlePost = document.querySelector();
+let urlString = window.location.search; // ?post=15
+let index = urlString.indexOf('='); // 5
+let postId = Number(urlString.slice(index + 1)); // string kopija nuo 5+1 elemento iki galo, paversta skaičiumi
 
-function renderPost(post){
-postContainer.innerHTML = `
-`
+function renderPost(data) {
+  postContainer.innerHTML = `
+    <img src="https://unsplash.it/1110/400" alt="" class="post__img">
+    <h1 class="post__header">${data.post.title}</h1>
+    <div class="user-info">
+    <a class="user-name" href="./userPosts.html?user=${data.user.id}">${data.user.name}</a>
+      <img src="https://unsplash.it/120/120">
+      <a class="user-email" href="mailto: ${data.user.email}">${data.user.email}</a>
+      <a class="user-phone" href="tel: ${data.user.phone}">${data.user.phone}</a>
+    </div>
+    <p class="post__body">${data.post.body}</p>
+    <hr>
+    <hr>`;
 }
 
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then(res => res.json())
+  .then((post) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      .then(res => res.json())
+      .then(user =>renderPost({ post, user }))
+  });
--- Kipras J/10 paskaita - crud/js/singlePost.js
+++ Kipras J/10 paskaita - crud/js/singlePost.js
@@ -1,16 +1,26 @@
 const postContainer = document.querySelector('.js-posts');
-let urlString = window.location.search;
-let index = urlString.indexOf('=');
-let postId = Number(urlString.slice(index + 1));
 
-function renderPosts(posts){
-    postContainer.innerHTML = `
-    <img src="https://unsplash.it/1140/400" alt="" class="post_image">
-        <h1 class="post_title">${posts.title}</h1>
-        <p class="post_text">${posts.body}</p>
-    `;
+let urlString = window.location.search; // ?post=15
+let index = urlString.indexOf('='); // 5
+let postId = Number(urlString.slice(index + 1)); // string kopija nuo 5+1 elemento iki galo, paversta skaičiumi
+
+function renderPost(data) {
+  postContainer.innerHTML = `
+    <img src="https://unsplash.it/1110/400" alt="" class="post_image">
+    <h1 class="post_title">${data.post.title}</h1>
+    <div class="user-info">
+    <a class="user-name" href="./userPosts.html?user=${data.user.id}">${data.user.name}</a>
+    <img src="https://unsplash.it/80/80">
+    <a class="user-email" href="mailto: ${data.user.email}">${data.user.email}</a>
+    <a class="user-phone" href="tel: ${data.user.phone}">${data.user.phone}</a>
+    </div>  
+    <p class="post_text">${data.post.body}!</p> `;
 }
 
 fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    .then(res => res.json())
-    .then(renderPosts);
+  .then(res => res.json())
+  .then((post) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      .then(res => res.json())
+      .then(user =>renderPost({ post, user }))
+  });
