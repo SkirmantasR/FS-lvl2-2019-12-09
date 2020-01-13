0ca2773a771378a09ebfd33111f5ea9d7d9c740f
Zilwa <zilvinas.vidmantas.93@gmail.com>
2020-01-13 18:36:07 +0200

    added git ignore

---
+++ Tomas A/13 paskaita/js/libs/LoginManager.js
@@ -0,0 +1,37 @@
+class LoginManager {
+  constructor() {
+
+  }
+
+  formObjectFromform = (form) => {
+    let formInputs = Array.from(form).slice(0, -1);
+    let formData = {};
+    formInputs.forEach(input => {
+      if (input.type == 'checkbox') {
+        formData[input.id] = input.checked;
+      } else formData[input.id] = input.value;
+    });
+    return formData;
+  }
+
+  register = ({login, password}) => {
+    let lastUserId  = JSON.parse(localStorage.getItem('lastUserId'));
+    let users = JSON.parse(localStorage.getItem('users'));
+    users.push({
+      id: ++lastUserId,
+      login,
+      password
+    });
+    localStorage.setItem('lastUserId',JSON.stringify(lastUserId))
+    localStorage.setItem('users',JSON.stringify(users));
+    
+  }
+}
+
+
+const loginManager = new LoginManager();
+
+// Duomenų įdejimas į Naršyklės atmintį - localStorage
+// localStorage.setItem('lastUserId', JSON.stringify(0));
+// localStorage.setItem('lastUserId', JSON.stringify(0));
+// localStorage.setItem('users', JSON.stringify([]));
---
+++ Tomas A/13 paskaita/js/libs/Validation.js
@@ -0,0 +1,67 @@
+class Validation {
+  constructor(data) {
+    this.data = data;
+    this.errors = {};
+    for (const key in data) {
+      this.errors[key] = [];
+    }
+  }
+
+  minLength = (key, len) => {
+    if (this.data[key].length < len) {
+      this.errors[key].push(`${key} must be at least ${len} chars.`)
+    }
+    return this;
+  }
+
+  maxLength = (key, len) => {
+    if (this.data[key].length > len) {
+      this.errors[key].push(`${key} must be no more than ${len} chars.`)
+    }
+    return this;
+  }
+
+  uniqueLogin = () => {
+    const users = JSON.parse(localStorage.getItem('users'));
+    const logins = users.map(user => user.login);
+    let unique = !logins.includes(this.data.login);
+    if (!unique) this.errors['login'].push('Login in not unique');
+    return this;
+  }
+
+  equalPasswords = () => {
+    if (this.data.password !== this.data['rep-password']) {
+      this.errors.password.push('Passwords do not match');
+      this.errors['rep-password'].push('Passwords do not match');
+    }
+    return this;
+  }
+
+  hasErrors = () => {
+    for (let key in this.errors) {
+      if (this.errors[key].length > 0) return true;
+    }
+    return false;
+  }
+
+  printErrors = () => {
+    for (const key in this.errors) {
+      if(this.errors[key].length > 0){
+        let input = document.querySelector('#' + key);
+        let errorField = document.querySelector(`#${key}~.invalid-feedback`);
+        input.classList.add('is-invalid');
+        this.errors[key].forEach(msg => {
+          errorField.innerHTML += `<div>${msg}</div>`;
+        });
+        input.addEventListener('keypress', this.removeErrors)
+      }
+    }
+  }
+
+  removeErrors(e){
+    e.target.classList.remove('is-invalid');
+    e.target.parentNode.querySelector('.invalid-feedback').innerHTML = '';
+    e.target.removeEventListener('keypress', this.removeErrors);
+  }
+}
+
