c80fa24cb99bd725aa16f0e3e0a6dde0c2e93dac
Zilwa <zilvinas.vidmantas.93@gmail.com>
2020-01-13 21:35:15 +0200

    answers

--- Žilvinas Vidmantas/13 paskaita - node npm webpack/dist/Logger.js
+++ Žilvinas Vidmantas/13 paskaita - node npm webpack/dist/Logger.js
@@ -2,12 +2,14 @@ class Logger{
     constructor(input){
         this.input = input;
     }
-
-    logError(msgArr) {
-        
+    logErrors(msgArr) {
+        this.input.innerHTML = '<h3 class="text-danger">Error:</h3>';
+        msgArr.forEach(msg => {
+            this.input.innerHTML += `<div class="text-danger">${msg}</div>`;
+        });
     }
     
-    logAnswer(msg) {
-        
+    logAnswer(ans) {
+        this.input.innerHTML = `<h3 class="text-success">Answer is: ${ans}</h3>`;
     }
 }
--- Žilvinas Vidmantas/13 paskaita - node npm webpack/dist/validation.js
+++ Žilvinas Vidmantas/13 paskaita - node npm webpack/dist/validation.js
@@ -1,26 +1,23 @@
-class Validation{
-    constructor(inputName, input){
-        this.name = inputName;
-        this.input = input
-        this.errors = [];
-    }
-
-    isNumber(){
-      // ...
-        return this;
-    }
-
-    length(min, max){
-      // ...
-        return this;
-    }
-
-    notEmpty(){
-      // ...
+class Validation {
+  constructor(inputName, input) {
+    this.name = inputName;
+    this.input = input
+    this.errors = [];
+  }
+  isNumber() {
+    if (isNaN(this.input)) this.errors.push(`${this.name}: value is not numerical.`);
+    return this;
+  }
+  length(min, max) {
+    if (this.input.length < min || this.input.length > max)
+      this.errors.push(`${this.name}: must be between ${min} and ${max}.`);
+    return this;
+  }
+  notEmpty() {
+    if (this.input == '') this.errors.push(`${this.name}: must not be empty.`);
       return this;
-    }
-
-    getErrors(){
-      // ...
-    }
+  }
+  getErrors() {
+    return this.errors;
+  }
 }
