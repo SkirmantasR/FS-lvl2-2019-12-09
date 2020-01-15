7215cb80bb13b4e03d592381af9295a0b259b356
Zilwa <zilvinas.vidmantas.93@gmail.com>
2020-01-15 21:36:45 +0200

    webpack styles

--- Erika K/14 paskaita - webpack/src/index.js
+++ Erika K/14 paskaita - webpack/src/index.js
@@ -1,3 +1,4 @@
 import App from './js/App';
+import styles from './scss/main.scss';
 
 new App().start();
--- Tomas A/14 paskaita - webpack/webpack.config.js
+++ Tomas A/14 paskaita - webpack/webpack.config.js
@@ -1,4 +1,10 @@
-module.exports= {
+const path = require('path');
+
+module.exports = {
     mode: "development",
-    entry: "./src/index.js"
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    }
 }
